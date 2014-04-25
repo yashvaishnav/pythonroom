f959a910100391843d43c2466a82d83e1fb96062
yashvaishnav <yashvaishnav@gmail.com>
2014-04-25 16:31:12 -0700

    for loop

--- draw.py
+++ draw.py
@@ -1,1 +1,15 @@
 # author: yashvaishnav
+
+import turtle
+
+length = 50
+angle = 36
+numbers = range(0, 10)
+
+yash = turtle.Turtle()
+
+for number in numbers:
+	yash.color("#43709F")
+	yash.forward(length)
+	yash.left(angle)
+
