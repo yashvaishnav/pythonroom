9172f786bdd9314d7c70ff61ae0986e80a0a1f20
yashvaishnav <yashvaishnav@gmail.com>
2014-04-26 13:18:59 -0700

    learning copy+paste

--- turtledraw.py
+++ turtledraw.py
@@ -1,1 +1,19 @@
 # author: yashvaishnav
+
+import turtle
+
+length = 100
+angle = 72
+
+yash = turtle.Turtle()
+
+yash.forward(length)
+yash.left(angle)
+yash.forward(length)
+yash.left(angle)
+yash.forward(length)
+yash.left(angle)
+yash.forward(length)
+yash.left(angle)
+yash.forward(length)
+yash.left(angle)
