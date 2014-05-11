2f1878c66cc88ef625cd04eaa108338d388d5dc7
yashvaishnav <yashvaishnav@gmail.com>
2014-05-11 15:23:12 -0700

    The sum of the prime numbers between 1 and 1000.

--- primesum.py
+++ primesum.py
@@ -1,5 +1,13 @@
-n = 504
-tests = range(2, 503)
-for t in tests:
-	if n % t == 0:
-		print "Not prime."
+total = 0
+lastPrimeISaw = 2
+for n in range(3, 1001):
+	tests = range(2, n - 1)
+	prime = True
+	for t in tests:
+		if n % t == 0:
+			prime = False
+	
+	if prime == True:
+		total = total + n
+
+print total
